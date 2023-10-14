8b68f84ec247c4057a3cd60d3ad4753c6e897d6a
parthiv <parmarparthiv77@gmail.com>
2023-10-14 19:13:06 +0530

    configuring the database connection

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,8 @@
 import express from 'express';
+import bodyParser from 'body-parser';
+import cookieParser from 'cookie-parser';
+import cors from 'cors';
+import { MongoClient, ServerApiVersion } from 'mongodb';
 import dotenv from 'dotenv';
 
 dotenv.config();
@@ -7,6 +11,38 @@ const port = process.env.PORT;
 
 const app = express();
 
+app.use(cors({
+    credentials: true,
+}));
+
+app.use(cookieParser());
+app.use(bodyParser.json());
+
 app.listen(port, () => {
     console.log(`Server is listening on : http://localhost:${port}`);
 });
+
+const uri: any = process.env.DATABASE_URL;
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+    serverApi: {
+        version: ServerApiVersion.v1,
+        strict: true,
+        deprecationErrors: true,
+    }
+});
+
+async function run() {
+    try {
+        // Connect the client to the server	(optional starting in v4.7)
+        await client.connect();
+        // Send a ping to confirm a successful connection
+        await client.db("admin").command({ ping: 1 });
+        console.log("You successfully connected to MongoDB!");
+    } finally {
+        // Ensures that the client will close when you finish/error
+        await client.close();
+    }
+}
+run().catch(console.dir);
